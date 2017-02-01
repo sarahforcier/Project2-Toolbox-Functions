088f8b13bbe00e13d9e9be2a02abe79e4a678d7f
Sarah <sarahforcier@users.noreply.github.com>
2017-01-31 21:04:03 -0500

    big fix 2

--- src/main.js
+++ src/main.js
@@ -99,7 +99,7 @@ function onLoad(framework) {
 
     // set skybox
     var loader = new THREE.CubeTextureLoader();
-    var urlPrefix = '/images/skymap/';
+    var urlPrefix = 'images/skymap/';
 
     var skymap = new THREE.CubeTextureLoader().load([
         urlPrefix + 'px.jpg', urlPrefix + 'nx.jpg',
@@ -111,7 +111,7 @@ function onLoad(framework) {
 
     // load a simple obj mesh
     var objLoader = new THREE.OBJLoader();
-    var obj = objLoader.load('/geo/feather_new.obj', function(obj) {
+    var obj = objLoader.load('geo/feather_new.obj', function(obj) {
         // LOOK: This function runs after the obj has finished loading
         featherGeo = obj.children[0].geometry;
         createWing(scene, featherGeo);
